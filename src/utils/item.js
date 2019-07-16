eab22d39d1bab0832f314700f8d9fbf5a229d3e8
Rodrigo Gomes <prodrigo@poli.ufrj.br>
2019-07-16 14:51:49 -0300

    creating classes

---
+++ src/utils/item.js
@@ -0,0 +1,7 @@
+export default class Item {
+  constructor(name, imgUri, price) {
+    this.name = name;
+    this.imgUri = imgUri;
+    this.price = price;
+  }
+}
--- src/utils/items.js
+++ src/utils/items.js
@@ -1,37 +1,33 @@
+import Item from "./item";
 export default [
-  {
-    name: "Soccer Ball",
-    imgUri: "https://www.urbanize.com.ph/3500-big_default_2x/soccer-ball.jpg",
-    price: 54.0
-  },
-  {
-    name: "BasketBall",
-    imgUri:
-      "https://www.spalding.com/dw/image/v2/ABAH_PRD/on/demandware.static/-/Sites-masterCatalog_SPALDING/default/dwf0e15940/images/hi-res/74876E_SIDE.jpg?sw=555&sh=689&sm=cut",
-    price: 65.0
-  },
-  {
-    name: "VolleyBall",
-    imgUri:
-      "https://images-na.ssl-images-amazon.com/images/I/71kjs9OiBdL._SX425_.jpg",
-    price: 45.0
-  },
-  {
-    name: "FootBall",
-    imgUri:
-      "https://images-na.ssl-images-amazon.com/images/I/51OM%2BtVJ6AL.jpg",
-    price: 76.0
-  },
-  {
-    name: "GolfBall",
-    imgUri:
-      "https://contents.mediadecathlon.com/p1345747/800x800/sq/distance_100_golf_ball_x12_-_white_inesis_golf_8334028_1345747.jpg?k=8bfca050583b3bfb22ace6db8769f696",
-    price: 26.0
-  },
-  {
-    name: "BeiseBall",
-    imgUri:
-      "https://cdn.shopify.com/s/files/1/0097/7152/products/BN-200-USSSA_1800x1800.jpg?v=1475791778",
-    price: 59.0
-  }
+  new Item(
+    "Soccer Ball",
+    "https://www.urbanize.com.ph/3500-big_default_2x/soccer-ball.jpg",
+    54.0
+  ),
+  new Item(
+    "BasketBall",
+    "https://www.spalding.com/dw/image/v2/ABAH_PRD/on/demandware.static/-/Sites-masterCatalog_SPALDING/default/dwf0e15940/images/hi-res/74876E_SIDE.jpg?sw=555&sh=689&sm=cut",
+    65.0
+  ),
+  new Item(
+    "VolleyBall",
+    "https://images-na.ssl-images-amazon.com/images/I/71kjs9OiBdL._SX425_.jpg",
+    45.0
+  ),
+  new Item(
+    "FootBall",
+    "https://images-na.ssl-images-amazon.com/images/I/51OM%2BtVJ6AL.jpg",
+    79.0
+  ),
+  new Item(
+    "GolfBall",
+    "https://contents.mediadecathlon.com/p1345747/800x800/sq/distance_100_golf_ball_x12_-_white_inesis_golf_8334028_1345747.jpg?k=8bfca050583b3bfb22ace6db8769f696",
+    26.0
+  ),
+  new Item(
+    "BeiseBall",
+    "https://contents.mediadecathlon.com/p1345747/800x800/sq/distance_100_golf_ball_x12_-_white_inesis_golf_8334028_1345747.jpg?k=8bfca050583b3bfb22ace6db8769f696",
+    59.0
+  )
 ];
